a5d51e76611b17fadde6d28e4f7182fa8e996a34
441858299 <441858299@qq.com>
2022-05-11 14:53:06 +0800

    add bgy-anchor

---
+++ docs/examples/bgy-anchor/Bcontainer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div style="display: flex">
+    <div
+      id="IDBig"
+      ref="containerRef"
+      style="flex: 10; margin-right: 30px; height: 500px; overflow-y: auto"
+    >
+      <div id="content-11" style="height: 200px; background-color: skyblue">
+        anchor-content-1
+      </div>
+      <div id="content-12" style="height: 200px; background-color: greenyellow">
+        anchor-content-2
+      </div>
+      <div id="content-13" style="height: 200px; background-color: #1f2f3d">
+        anchor-content-3
+      </div>
+      <div id="content-14" style="height: 200px; background-color: hotpink">
+        anchor-content-4
+      </div>
+      <div id="content-15" style="height: 200px; background-color: orange">
+        anchor-content-5
+      </div>
+    </div>
+    <el-bgy-anchor :offset="120" :get-container="getContainer" style="flex: 7">
+      <el-bgy-anchor-link type="default" href="#content-11" title="基础锚点" />
+      <el-bgy-anchor-link type="default" href="#content-12" title="多级锚点" />
+      <el-bgy-anchor-link
+        type="default"
+        href="#content-13"
+        title="指定容器锚点"
+      />
+      <el-bgy-anchor-link
+        type="default"
+        href="#content-14"
+        title="特定交互锚点"
+      />
+      <el-bgy-anchor-link
+        type="default"
+        href="#content-15"
+        title="不同尺寸的锚点"
+      />
+      <el-bgy-anchor-link type="default" href="#content-15"
+        >我是自定义title</el-bgy-anchor-link
+      >
+    </el-bgy-anchor>
+  </div>
+</template>
---
+++ docs/examples/bgy-anchor/basic.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-bgy-anchor :offset="10">
+    <el-bgy-anchor-link type="default" href="#content-1" title="基础锚点" />
+    <el-bgy-anchor-link type="default" href="#content-2" title="多级锚点" />
+    <el-bgy-anchor-link type="default" href="#content-3" title="指定容器锚点" />
+    <el-bgy-anchor-link type="default" href="#content-4" title="特定交互锚点" />
+    <el-bgy-anchor-link
+      type="default"
+      href="#content-5"
+      title="不同尺寸的锚点"
+    />
+  </el-bgy-anchor>
+</template>
